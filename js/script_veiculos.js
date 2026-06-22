980e57187f67dd992f4d4e30d460094a37906752
Miguel Rinaldo <miguel.r.nascimento9@aluno.senai.br>
2026-06-22 20:32:07 -0300

    adicionei vários comandos no js/

--- js/script_veiculos.js
+++ js/script_veiculos.js
@@ -3,3 +3,16 @@ import { calcularIPVA} from "./script_calculo.js"
 const form = document.querySelector("form")
 
 const lista = document.querySelector("#lista-veiculos")
+
+form.addEventListener("submit", function(evt) {
+evt.preventDefault(); 
+})
+
+const modelo = document.querySelector("modelo").value;
+const marca = document.querySelector("marca").value;
+const placa = document.querySelector("placa").value;
+const ano = document.querySelector("ano").value;
+const valor = document.querySelector("valor").value;
+
+// pega o radio que foi selecionado
+const combustivel = document.querySelector("input[name='combustivel']:checked").value;
