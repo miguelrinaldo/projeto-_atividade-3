95cec9c9c4cd60c1f8d598b49a85e01d709e38b3
Miguel Rinaldo <miguel.r.nascimento9@aluno.senai.br>
2026-06-22 19:48:59 -0300

    adicionei vários comandos no js/

--- js/script_veiculos.js
+++ js/script_veiculos.js
@@ -1,1 +1,5 @@
-import {taxaIpva} from "./script_calculo.js"
+import { calcularIPVA} from "./script_calculo.js"
+
+const form = document.querySelector("form")
+
+const lista = document.querySelector("#lista-veiculos")
